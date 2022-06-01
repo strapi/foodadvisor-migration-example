d2e152ba40b4d4690d1dc40bb0269a39b8f65bcb
Bassel Kanso <basselkanso82@gmail.com>
2022-06-01 12:07:51 +0300

    create two separate directories for V3 and V4

---
+++ V4/api/restaurant/config/schema.graphql.js
@@ -0,0 +1,12 @@
+module.exports = {
+  definition: /* GraphQL */ `
+    extend type Restaurant {
+      note: Float
+      noteDetails: [RestaurantNote!]!
+    }
+    type RestaurantNote {
+      note: Int
+      count: Int
+    }
+  `,
+};
---
+++ V4/api/review/controllers/Review.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * Review.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Review`.
+ */
+
+module.exports = {};
